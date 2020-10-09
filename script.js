bad8d1299d12aa53561e07caf3251a9a045b9cf2
olivialoh77 <olivialoh77@gmail.com>
2020-10-08 23:45:25 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+
+$(document).ready(function() {
+  
+  $("#tab1_content").hide();
+  $("#tab2_content").show();
+  $("#tab3_content").hide();
+  $("#tab4_content").hide();
+
+  $("#tab1").click(function() {
+   
+    $("#tab1_content").show();
+    $("#tab2_content").hide();
+    $("#tab3_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab2").click(function() {
+   
+    $("#tab2_content").show();
+    $("#tab1_content").hide();
+    $("#tab3_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab3").click(function() {
+    
+    $("#tab3_content").show();
+    $("#tab1_content").hide();
+    $("#tab2_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab4").click(function() {
+   
+    $("#tab4_content").show();
+    $("#tab1_content").hide();
+    $("#tab2_content").hide();
+    $("#tab3_content").hide();
+  });
+});
---
+++ testscript.js
@@ -0,0 +1,22 @@
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+}, 3000);
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
+
